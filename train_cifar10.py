cf92dff4dfcfdc01cf9e4a386ba824773118bed3
oneonlee <dg.lee@postech.ac.kr>
2024-04-02 21:51:42 +0900

    implement num_noised_class

--- train_cifar10.py
+++ train_cifar10.py
@@ -20,6 +20,7 @@ parser.add_argument('--train_batch_size', type=int, default=128, help='train_bat
 parser.add_argument('--train_epoch', type=int, default=4000, help='train_epoch')
 parser.add_argument('--eval_batch_size', type=int, default=256, help='eval_batch_size')
 parser.add_argument('--label_noise', type=float, default=0.15, help='label_noise')
+parser.add_argument('--num_noised_class', type=int, default=10, help='The number of classes that will receive noise. (0 < num_noised_class <= # class)')
 parser.add_argument('--k', type=int, default=64, help='1 to k iteration')
 
 args = parser.parse_args()
@@ -64,15 +65,49 @@ for k in range(1,65):
     train_data = dataset(f'{data_dir}', train=True, transform=train_transform,download=True)
     train_targets = np.array(train_data.targets)
     data_size = len(train_targets)
-    random_index = random.sample(range(data_size), int(data_size*args.label_noise))
-    random_part = train_targets[random_index]
-    np.random.shuffle(random_part)
-    train_targets[random_index] = random_part
-    train_data.targets = train_targets.tolist()
 
-    noise_data = dataset(f'{data_dir}', train=True, transform=train_transform)
-    noise_data.targets = random_part.tolist()
-    noise_data.data = train_data.data[random_index]
+    all_class = np.unique(train_targets) # list of all unique classes
+    num_class = len(all_class) # number of classes
+
+    # noising data
+    assert args.num_noised_class <= num_class, "num_noised_class must be less than or equal to the number of classes."
+    assert args.num_noised_class > 0, "num_noised_class must be greater than 0."
+    num_random_elements = int(data_size*args.label_noise) # number of elements that should be noised
+
+    if args.num_noised_class == num_class:
+        random_index = random.sample(range(data_size), num_random_elements)
+        random_part = train_targets[random_index]
+        np.random.shuffle(random_part)
+        train_targets[random_index] = random_part
+        train_data.targets = train_targets.tolist()
+
+        noise_data = dataset(f'{data_dir}', train=True, transform=train_transform)
+        noise_data.targets = random_part.tolist()
+        noise_data.data = train_data.data[random_index]
+    else: # 0 < num_noised_class < num_class
+        candidate_class = random.sample(list(all_class), args.num_noised_class) # randomly selected classes that will receive noise
+
+        candidate_random_index = []
+        candidate_random_part = []
+        for i in candidate_class:
+            candidate_random_index.extend(np.where(train_targets == i)[0].tolist()) # index of the selected class
+            candidate_random_part.extend(train_targets[np.where(train_targets == i)[0]].tolist()) # targets of the selected class
+        
+        assert len(candidate_random_index) == len(candidate_random_part), "The number of index and targets must be the same."
+        assert len(candidate_random_index) >= num_random_elements, \
+            f"When the total number of dataset is {data_size} and `label_noise` is {args.label_noise}, the number of noised elements would be {num_random_elements}, but the number of {args.num_noised_class} classes of data is {len(candidate_random_index)}. \
+                You should increase the `num_noised_class` or decrease the `label_noise`."
+
+        random_index = random.sample(candidate_random_index, num_random_elements)
+        random_part = train_targets[random_index]
+        random_part = np.array(random_part)
+        np.random.shuffle(random_part)
+        train_targets[random_index] = random_part
+        train_data.targets = train_targets.tolist()
+
+        noise_data = dataset(f'{data_dir}', train=True, transform=train_transform)
+        noise_data.targets = random_part.tolist()
+        noise_data.data = train_data.data[random_index]
 
 
     test_data = dataset(f'{data_dir}', train=False, transform=eval_transform)
