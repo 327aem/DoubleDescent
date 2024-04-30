c1eeaf91bd1b8d4b2997d0ea7521ae5da20ca946
Dongha Kim <dhkim0317@postech.ac.kr>
2024-04-30 20:21:15 +0900

    imbalacned dataset initialize

--- train_cifar10.py
+++ train_cifar10.py
@@ -9,6 +9,7 @@ import random
 import os
 from tqdm import tqdm
 import wandb
+from utils.data_preprocessing.imbalancing import *
 
 parser = argparse.ArgumentParser()
 # dataset part
@@ -22,6 +23,12 @@ parser.add_argument('--label_noise', type=float, default=0.15, help='label_noise
 parser.add_argument('--num_noised_class', type=int, default=10, help='The number of classes that will receive noise. (0 < num_noised_class <= # class)')
 parser.add_argument('--img_noise', type=str, default=None, help='None , Partial , All')
 parser.add_argument('--k', type=int, default=64, help='1 to k iteration')
+parser.add_argument('--is_lmbalance', type=bool, default=False, help='True, False')
+parser.add_argument('--imb_type', default="exp", type=str, help='imbalance type')
+parser.add_argument('--imb_factor', default=0.01, type=float, help='imbalance factor')
+parser.add_argument('--train_rule', default='None', type=str, choices=['None', 'Resample', 'Reweight', 'DRW'])
+parser.add_argument('--rand_number', default=0, type=int, help='fix random number for data sampling')
+parser.add_argument('--extra_name', default='imbalanced', type=str, help='(additional) name to indicate experiment')
 
 args = parser.parse_args()
 
@@ -62,19 +69,65 @@ def gauss_noise_tensor(img):
         
     return out
 
-dataset = datasets.CIFAR10
-noise_transform = transforms.Compose([
-                                gauss_noise_tensor,
-                                ])
+# dataset = datasets.CIFAR10
+
+"""
+test_data = dataset(f'{data_dir}', train=False, transform=eval_transform)
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=args.train_batch_size, shuffle=True, num_workers=0,
+                                        drop_last=False)
+noise_loader = torch.utils.data.DataLoader(noise_data, batch_size=args.train_batch_size, shuffle=False, num_workers=0,
+                                        drop_last=False)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=args.eval_batch_size, shuffle=False, num_workers=0,
+                                        drop_last=False)
+"""
+
+mean = [0.4914, 0.4822, 0.4465] if args.dataset.startswith('cifar') else [.5, .5, .5]
+std = [0.2023, 0.1994, 0.2010] if args.dataset.startswith('cifar') else [.5, .5, .5]
+# std = [0.2470, 0.2435, 0.2616] if args.dataset.startswith('cifar') else [.5, .5, .5]
+# 이게 맞는 std 아닌가;
+
+noise_transform = transforms.Compose([gauss_noise_tensor,])
 train_transform = transforms.Compose([transforms.RandomCrop(32, padding=4),
                                 transforms.RandomHorizontalFlip(),
-                                transforms.ToTensor()])
+                                transforms.ToTensor(),
+                                transforms.Normalize(mean, std),])
 train_noise_transform = transforms.Compose([transforms.RandomCrop(32, padding=4),
                                 transforms.RandomHorizontalFlip(),
                                 transforms.ToTensor(),
                                 gauss_noise_tensor,])
-eval_transform = transforms.Compose([transforms.ToTensor()])
+eval_transform = transforms.Compose([transforms.ToTensor(),
+                                     transforms.Normalize(mean, std),])
 
+if args.data_name == 'cifar10':
+    train_dataset = ImbalanceCIFAR10(
+        root=args.data_path, imb_type=args.imb_type, imb_factor=args.imb_factor,
+        rand_number=args.rand_number, train=True, download=True, transform=train_transform)
+    test_dataset = datasets.CIFAR10(root=args.data_path,
+                                    train=False, download=True, transform=eval_transform)
+    train_sampler = None
+    if args.train_rule == 'Resample':
+        train_sampler = ImbalancedDatasetSampler(train_dataset)
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=args.train_batch_size, shuffle=(train_sampler is None),
+        num_workers=args.workers, pin_memory=True, sampler=train_sampler)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.eval_batch_size, shuffle=False,
+                                                num_workers=0, drop_last=False)
+elif args.data_name == 'cifar100':
+    train_dataset = ImbalanceCIFAR100(
+        root=args.data_path, imb_type=args.imb_type, imb_factor=args.imb_factor,
+        rand_number=args.rand_number, train=True, download=True, transform=train_transform)
+    test_dataset = datasets.CIFAR100(root=args.data_path,
+                                    train=False, download=True, transform=eval_transform)
+    train_sampler = None
+    if args.train_rule == 'Resample':
+        train_sampler = ImbalancedDatasetSampler(train_dataset)
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=args.train_batch_size, shuffle=(train_sampler is None),
+        num_workers=args.workers, pin_memory=True, sampler=train_sampler)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.eval_batch_size, shuffle=False,
+                                                num_workers=0, drop_last=False)
+else:
+    raise NotImplementedError(f"Dataset {args.data_name} is not supported!")
 
 for k in range(1,65):
     print(f"\nTrain K={k} Start!\n")
@@ -84,98 +137,44 @@ for k in range(1,65):
         model = resnet.resnet18(k=k)
     else:
         raise Exception("No such model!")
-
-    # load data
-    if args.img_noise == "All":
-        train_data = dataset(f'{data_dir}', train=True, transform=train_noise_transform,download=True)
-    else : 
-        train_data = dataset(f'{data_dir}', train=True, transform=train_transform,download=True)
-
-    train_targets = np.array(train_data.targets)
-    data_size = len(train_targets)
-
-    all_class = np.unique(train_targets) # list of all unique classes
-    num_class = len(all_class) # number of classes
-
-    # noising data
-    assert args.num_noised_class <= num_class, "num_noised_class must be less than or equal to the number of classes."
-    assert args.num_noised_class > 0, "num_noised_class must be greater than 0."
-    num_random_elements = int(data_size*args.label_noise) # number of elements that should be noised
-
-
-    ### All Dataset Labels are noised ###
-    if args.num_noised_class == num_class:
-        random_index = random.sample(range(data_size), num_random_elements)
-        random_part = train_targets[random_index]
-        np.random.shuffle(random_part)
-        train_targets[random_index] = random_part
-        train_data.targets = train_targets.tolist()
-        noise_data = dataset(f'{data_dir}', train=True, transform=train_transform)
-        noise_data.targets = random_part.tolist()
-        noise_data.data = train_data.data[random_index]
-    else: # 0 < num_noised_class < num_class
-        candidate_class = random.sample(list(all_class), args.num_noised_class) # randomly selected classes that will receive noise
-
-        candidate_random_index = []
-        candidate_random_part = []
-        for i in candidate_class:
-            candidate_random_index.extend(np.where(train_targets == i)[0].tolist()) # index of the selected class
-            candidate_random_part.extend(train_targets[np.where(train_targets == i)[0]].tolist()) # targets of the selected class
-        
-        assert len(candidate_random_index) == len(candidate_random_part), "The number of index and targets must be the same."
-        assert len(candidate_random_index) >= num_random_elements, \
-            f"When the total number of dataset is {data_size} and `label_noise` is {args.label_noise}, the number of noised elements would be {num_random_elements}, but the number of {args.num_noised_class} classes of data is {len(candidate_random_index)}. \
-                You should increase the `num_noised_class` or decrease the `label_noise`."
-
-        random_index = random.sample(candidate_random_index, num_random_elements)
-        random_part = train_targets[random_index]
-
-        if args.img_noise == "Partial":
-            random_img = train_data.data[random_index] # Select image in noised classes
-            # Add Gaussian Noise only about selected noise classes
-            for idx,img in enumerate(random_img):
-                img = noise_transform(torch.tensor(img))
-                random_img[idx] = img
-        
-        random_part = np.array(random_part)
-        np.random.shuffle(random_part)
-        train_targets[random_index] = random_part
-        train_data.targets = train_targets.tolist()
-        if args.img_noise == "Partial":
-            train_data.data[random_index] = random_img
-
-        noise_data = dataset(f'{data_dir}', train=True, transform=train_transform)
-        noise_data.targets = random_part.tolist()
-        noise_data.data = train_data.data[random_index]
-
-
-    test_data = dataset(f'{data_dir}', train=False, transform=eval_transform)
-    train_loader = torch.utils.data.DataLoader(train_data, batch_size=args.train_batch_size, shuffle=True, num_workers=0,
-                                            drop_last=False)
-    noise_loader = torch.utils.data.DataLoader(noise_data, batch_size=args.train_batch_size, shuffle=False, num_workers=0,
-                                            drop_last=False)
-    test_loader = torch.utils.data.DataLoader(test_data, batch_size=args.eval_batch_size, shuffle=False, num_workers=0,
-                                            drop_last=False)
-
+    
     # build model
     device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
     model = model.to(device)
-
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
-    wrapper = ModelWrapper(model, optimizer, criterion, device)
 
     # train the model
     save_path = os.path.join('runs', args.data_name, "{}_{}_k{}".format(args.model_name, int(args.label_noise*100), k))
     if not os.path.exists(save_path):
         os.makedirs(save_path)
-    np.savez(os.path.join(save_path, "label_noise.npz"), index=random_index, value=random_part)
+    # np.savez(os.path.join(save_path, "label_noise.npz"), index=random_index, value=random_part)
 
     itr_index = 1
-    wrapper.train()
+
+    cls_num_list = train_dataset.get_cls_num_list()
+
+    # train loop
+    if args.train_rule == 'Reweight':
+        beta = 0.9999
+        effective_num = 1.0 - np.power(beta, cls_num_list)
+        per_cls_weights = (1.0 - beta) / np.array(effective_num)
+        per_cls_weights = per_cls_weights / np.sum(per_cls_weights) * len(cls_num_list)
+        per_cls_weights = torch.FloatTensor(per_cls_weights).cuda(args.gpu)
+    elif args.train_rule == 'DRW':
+        idx = id_epoch // 160
+        betas = [0, 0.9999]
+        effective_num = 1.0 - np.power(betas[idx], cls_num_list)
+        per_cls_weights = (1.0 - betas[idx]) / np.array(effective_num)
+        per_cls_weights = per_cls_weights / np.sum(per_cls_weights) * len(cls_num_list)
+        per_cls_weights = torch.FloatTensor(per_cls_weights).cuda(args.gpu)
+    else:
+        per_cls_weights = None
+
+    criterion = torch.nn.CrossEntropyLoss(weight=per_cls_weights)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    wrapper = ModelWrapper(model, optimizer, criterion, device)
 
     for id_epoch in tqdm(range(args.train_epoch)):
-        # train loop
+        wrapper.train()
 
         for id_batch, (inputs, targets) in enumerate(train_loader):
 
@@ -184,10 +183,8 @@ for k in range(1,65):
 
         wrapper.eval()
         test_loss, test_acc = wrapper.eval_all(test_loader)
-        noise_loss, noise_acc = wrapper.eval_all(noise_loader)
         print("epoch:{}/{}, batch:{}/{}, testing...".format(id_epoch + 1, args.train_epoch, id_batch + 1, len(train_loader)))
         print("clean: loss={}, acc={}".format(test_loss, test_acc))
-        print("noise: loss={}, acc={}".format(noise_loss, noise_acc))
         print()
 
         state = {
@@ -199,16 +196,12 @@ for k in range(1,65):
         }
         torch.save(state, os.path.join(save_path, "ckpt.pkl"))
         # return to train state.
-        wrapper.train()
+        
     result_test_loss, result_test_acc = wrapper.eval_all(test_loader)
-    result_noise_loss, result_noise_acc = wrapper.eval_all(noise_loader)
     print(f"#### K : {k}-Evaluation ####")
     print("epoch:{}/{}, batch:{}/{}, testing...".format(id_epoch + 1, args.train_epoch, id_batch + 1, len(train_loader)))
     print("clean: loss={}, acc={}".format(test_loss, test_acc))
-    print("noise: loss={}, acc={}".format(noise_loss, noise_acc))
 
     ###### wandb ######
     wandb.log({"DD_test acc" : result_test_acc}, step = k)
     wandb.log({"DD_test loss" : result_test_loss}, step = k)
-    wandb.log({"DD_noise acc" : result_noise_loss}, step = k)
-    wandb.log({"DD_noise loss" : result_noise_acc}, step = k)
