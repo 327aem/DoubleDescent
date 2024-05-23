c49208f593cc2bd8fd3b07dcc743822e2b802993
makdong <dhkim0317@postech.ac.kr>
2024-05-23 21:52:26 +0900

    Resample experiment done

--- scripts/exp3.sh
+++ scripts/exp3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-masterK=4
-numGPU=$1
+masterK=64
+numGPU=3
 
 chmod 755 /home/dhkim0317/DoubleDescent
 chmod 755 /home/dhkim0317/DoubleDescent/train_cifar10_imbalance.py
@@ -11,11 +11,12 @@ pwd
 
 for ((k=$masterK;k>=1;k--))
 do
-    touch resample001-k_$masterk.log
+    touch none001-k_$k.log
+    cat /dev/null > none001-k_$k.log
 
-    echo K: $k - Start Code
+    echo K: $k - Start Code on device $(($k % $numGPU))
 
-    CUDA_VISIBLE_DEVICES=$(($k % numGPU)) python -u train_cifar10_imbalance.py \
+    CUDA_VISIBLE_DEVICES=$(($k % $numGPU)) python -u train_cifar10_imbalance.py \
         --data_name cifar10 \
         --model_name resnet \
         --train_batch_size 128 \
@@ -24,10 +25,10 @@ do
         --imb_type exp \
         --imb_factor 0.01 \
         --rand_number 0 \
-        --train_rule Resample \
-	--extra_name imbalance_resample_imb001 > resample001-k_$masterk.log 2>&1 &
+        --train_rule None \
+	--extra_name none_resample_imb001 > none001-k_$k.log 2>&1 &
     # echo End Code
 
 done
 
-tail -f resample001-k_$masterk.log
+tail -f none001-k_$masterK.log
--- scripts/sbatch_exp3.sh
+++ scripts/sbatch_exp3.sh
@@ -3,28 +3,26 @@
 #SBATCH -J DDD-imbalanced
 #SBATCH -o /home/dhkim0317/DoubleDescent/scripts/log_sbatch_exp3_1.log # TO BE EDITED
 #SBATCH -e /home/dhkim0317/DoubleDescent/scripts/log_sbatch_exp3_1_error.log
-#SBATCH --time 1-00:00:00
+#SBATCH --time 3-00:00:00
 
-#SBATCH -p titanxp 
-#SBATCH --gres=gpu:2
+#SBATCH -p A100-80GB 
+#SBATCH --gres=gpu:3
 
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --tasks-per-node=1
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=16
 
 #SBATCH --comment ImbalDDD
 
-numGPU=4 # TO BE EDITED
+numGPU=3 # TO BE EDITED
 
 echo "numGPU $numGPU"
 
 echo "Activate conda"
+source /home/dhkim0317/anaconda3/etc/profile.d/conda.sh
 
-CONDA_BASE=$(conda info --base)
-source $CONDA_BASE/etc/profile.d/conda.sh
-
-conda activate deeplearning
+conda activate DL
 
 echo "Call start"
 chmod 755 /home/dhkim0317/DoubleDescent/scripts/exp3.sh
